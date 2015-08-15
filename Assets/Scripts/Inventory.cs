b992a0b74d1e15a9527d93884b007123acfd3543
Julia Ford <julia.m.ford@gmail.com>
2015-08-15 14:57:26 -0400

    Done with function headers

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -5,22 +5,19 @@ using System.Collections.Generic;
 public abstract class Inventory : MonoBehaviour {
 
 	private bool hasItemWeights;
-	public  bool HasItemWeights { get { return hasItemWeights; } };
+	public  bool HasItemWeights { get { return hasItemWeights; } }
 
 	private bool hasStackSizes;
-	public  bool HasStackSizes { get { return hasItemWeights; } };
+	public  bool HasStackSizes { get { return hasItemWeights; } }
 
 	public abstract List<InventoryItem> GetContents ();
 	public abstract List<InventoryItem> GetContents<T> ();
 	public abstract double GetTotalWeight ();
+	public abstract void Draw (Rect place);
 
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start () {	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update () { }
 }
